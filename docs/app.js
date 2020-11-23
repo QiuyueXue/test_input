4cd73f7bc66467cd413e4e77358e124f6531f89f
Qiuyue Xue <qiuyuexue@Qiuyues-MacBook-Pro.local>
2020-11-23 14:58:02 -0500

    fix js

--- docs/app.js
+++ docs/app.js
@@ -4,11 +4,6 @@ const stop = document.querySelector('.stop');
 const soundClips = document.querySelector('.sound-clips');
 const amplitudeCanvas = document.querySelector('.visualizer');
 const mainSection = document.querySelector('.main-controls');
-
-// disable stop button while not recording
-stop.disabled = true;
-
-// visualiser setup - create web audio api context and canvas
 let audioCtx;
 const amplitudeCanvasCtx = amplitudeCanvas.getContext("2d");
 

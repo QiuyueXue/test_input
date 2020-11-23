55c56ab5c790ba7b7e284fc706a3507338a1c82c
Qiuyue Xue <qiuyuexue@Qiuyues-MacBook-Pro.local>
2020-11-23 15:10:33 -0500

    change audio format

--- docs/app.js
+++ docs/app.js
@@ -128,12 +128,12 @@ function gotStream(stream) {
   rec.ondataavailable = e => {
     audioChunks.push(e.data);
     if (rec.state == "inactive"){
-      let blob = new Blob(audioChunks,{type:'audio/x-mpeg-3'});
+      let blob = new Blob(audioChunks,{'type':'audio/ogg; codecs=opus'});
       recordedAudio.src = URL.createObjectURL(blob);
       recordedAudio.controls=true;
       recordedAudio.autoplay=true;
       audioDownload.href = recordedAudio.src;
-      audioDownload.download = 'mp3';
+      audioDownload.download = 'myrecording.ogg';
       audioDownload.innerHTML = 'download';
     }
   }
